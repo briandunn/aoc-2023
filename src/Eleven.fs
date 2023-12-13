2514ddbbaa8dd67e84f39ab6bf31636b2ff37c21
Brian Dunn <briandunn@users.noreply.github.com>
2023-12-13 15:13:09 -0600

    expand

--- src/Eleven.fs
+++ src/Eleven.fs
@@ -1,2 +1,50 @@
 module Eleven
-  let one lines = 1
+
+let parse: string seq -> bool Grid.Grid =
+    Grid.parse
+    <| function
+        | '#' -> true
+        | '.' -> false
+        | _ -> failwith "invalid tile"
+
+let print =
+    Grid.rows
+    >> Seq.iter (
+        Seq.map (fun b -> if b then "#" else ".")
+        >> String.concat ""
+        >> printfn "%s"
+    )
+
+
+let expand =
+  let rowsToColumns = Grid.fromRows >> Grid.columns
+  let map (row: bool seq) =
+      if row |> Seq.contains true then
+          seq { row }
+      else
+          seq {
+              row
+              row
+          }
+
+  Grid.rows
+  >> Seq.map map
+  >> Seq.concat
+  >> rowsToColumns
+  >> Seq.map map
+  >> Seq.concat
+  >> Grid.fromColumns
+
+
+
+let one lines =
+    let image = parse lines
+
+
+    image
+    |> expand
+    |> print
+
+
+
+    0
--- src/Grid.fs
+++ src/Grid.fs
@@ -18,3 +18,50 @@ let parse (parse: char -> 'a) (lines: string seq) : 'a Grid =
     let init x y = Map.find (x, y) tiles
 
     Array2D.init (max Tuple.first + 1) (max Tuple.second + 1) init
+
+
+let indexes (predicate: 'a -> bool) (array: 'a Grid) =
+    seq {
+        for x in 0 .. Array2D.length1 array - 1 do
+            for y in 0 .. Array2D.length2 array - 1 do
+                if predicate <| Array2D.get array x y then
+                    (x, y)
+    }
+
+
+let rows (grid: 'a Grid) =
+    seq {
+        for y in 0 .. Array2D.length2 grid - 1 do
+            yield
+                seq {
+                    for x in 0 .. Array2D.length1 grid - 1 do
+                        yield Array2D.get grid x y
+                }
+    }
+
+let fromRows (rows: 'a seq seq) : 'a Grid =
+  let h = Seq.length rows
+  let w = rows |> Seq.head |> Seq.length
+
+  let init x y = rows |> Seq.item y |> Seq.item x
+
+  Array2D.init w h init
+
+let fromColumns (columns: 'a seq seq) : 'a Grid =
+  let w = Seq.length columns
+  let h = columns |> Seq.head |> Seq.length
+
+  let init x y = columns |> Seq.item x |> Seq.item y
+
+  Array2D.init w h init
+
+
+let columns (grid: 'a Grid) =
+    seq {
+        for x in 0 .. Array2D.length1 grid - 1 do
+            yield
+                seq {
+                    for y in 0 .. Array2D.length2 grid - 1 do
+                        yield Array2D.get grid x y
+                }
+    }
