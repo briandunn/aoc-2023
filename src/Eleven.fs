58ebc975ab422ce6f43117d3627ca2caf79fef13
Brian Dunn <briandunn@users.noreply.github.com>
2023-12-14 06:41:05 -0600

    11 1

--- src/Eleven.fs
+++ src/Eleven.fs
@@ -17,34 +17,45 @@ let print =
 
 
 let expand =
-  let rowsToColumns = Grid.fromRows >> Grid.columns
-  let map (row: bool seq) =
-      if row |> Seq.contains true then
-          seq { row }
-      else
-          seq {
-              row
-              row
-          }
+    let map (row: bool seq) =
+        if row |> Seq.contains true then
+            seq { row }
+        else
+            seq {
+                row
+                row
+            }
 
-  Grid.rows
-  >> Seq.map map
-  >> Seq.concat
-  >> rowsToColumns
-  >> Seq.map map
-  >> Seq.concat
-  >> Grid.fromColumns
-
-
-
-let one lines =
-    let image = parse lines
+    Grid.rows
+    >> Seq.map map
+    >> Seq.concat
+    >> Grid.fromRows
+    >> Grid.columns
+    >> Seq.map map
+    >> Seq.concat
+    >> Grid.fromColumns
 
 
-    image
-    |> expand
-    |> print
+let rec combinations n list =
+    match list with
+    | [] -> []
+    | list when n = 1 -> [ for item in list -> [ item ] ]
+    | head :: rest ->
+        (rest
+         |> combinations (n - 1)
+         |> List.map (fun l -> head :: l))
+        @ combinations n rest
 
+let shortestPath =
+    function
+    | [ (xa, ya); (xb, yb) ] -> abs (xb - xa) + abs (yb - ya) |> Some
+    | _ -> None
 
-
-    0
+let one: string seq -> int =
+    parse
+    >> expand
+    >> Grid.indexes id
+    >> Seq.toList
+    >> combinations 2
+    >> List.choose shortestPath
+    >> List.sum
