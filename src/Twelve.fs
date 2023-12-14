918f54b8cf82641c46598ba6de2d1fefa94c4378
Brian Dunn <briandunn@users.noreply.github.com>
2023-12-14 14:23:06 -0600

    12 1

--- src/Twelve.fs
+++ src/Twelve.fs
@@ -30,7 +30,63 @@ let parse: string seq -> Record seq =
 
     Seq.map map
 
-let one lines =
-    parse lines |> printfn "%A"
+let damagedGroupLengths: Status seq -> int list =
+    let fold (current, lengths) status =
+        match status with
+        | Damaged ->
+            match current with
+            | None -> (Some 1, lengths)
+            | Some n -> (Some(n + 1), lengths)
 
-    0
+        | _ ->
+            match current with
+            | None -> (None, lengths)
+            | Some n -> (None, n :: lengths)
+
+    Seq.fold fold (None, [])
+    >> function
+        | (None, lengths) -> lengths
+        | (Some n, lengths) -> n :: lengths
+    >> List.rev
+
+let satisfies
+    { statuses = statuses
+      damagedGroupLengths = lengths }
+    =
+    statuses
+    |> damagedGroupLengths
+    |> ((=) (Seq.toList lengths))
+
+let permute ({ statuses = statuses } as r) =
+    let slots =
+        statuses
+        |> Seq.filter ((=) Unknown)
+        |> Seq.length
+
+    let rec permute length options =
+        if length = 0 then
+            []
+
+        else if length = 1 then
+            List.map List.singleton options
+        else
+            options
+            |> (permute (length - 1))
+            |> List.map (fun permutation -> List.map (fun option -> option :: permutation) options)
+            |> List.concat
+
+    let substitute statuses permutation =
+        let fold (out, permutation) status =
+            match status with
+            | Unknown -> Seq.head permutation :: out, Seq.tail permutation
+            | status -> status :: out, permutation
+
+        Seq.fold fold ([], permutation) statuses |> Tuple.first |> List.rev
+
+    permute slots [ Damaged; Operational ]
+    |> List.map (fun permutation -> { r with statuses = substitute statuses permutation })
+
+let one lines =
+    parse lines
+    |> Seq.map (permute >> Seq.filter satisfies >> Seq.length)
+    |> Seq.sum
