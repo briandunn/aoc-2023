7c67dfc01be2748ad7e26f7a524e65a4617b9cfb
Brian Dunn <briandunn@users.noreply.github.com>
2024-01-01 14:07:21 -0600

    i did it.

--- src/Eighteen.fs
+++ src/Eighteen.fs
@@ -36,8 +36,6 @@ let print path =
     let minY = path |> Seq.map snd |> Seq.min
     let maxY = path |> Seq.map snd |> Seq.max
 
-    printfn "%A" path
-
     seq {
         for y in minY..maxY do
             for x in minX..maxX do
@@ -52,24 +50,95 @@ let print path =
 
 let range start stop f =
     if start > stop then
-        [for i in stop..start -> f i]
+        [ for i in stop..start -> f i ]
     else
-        [for i in start..stop -> f i] |> List.rev
-let move =
+        [ for i in start..stop -> f i ] |> List.rev
+
+let border =
     let fold path (direction, meters) =
-        let next (x,y) =
+        let next (x, y) =
             match direction with
             | Right -> range x (x + meters) (fun x -> (x, y))
             | Left -> range x (x - meters) (fun x -> (x, y))
-            | Up -> range y (y + meters) (fun y -> (x, y))
-            | Down -> range y (y - meters) (fun y -> (x, y))
+            | Up -> range y (y - meters) (fun y -> (x, y))
+            | Down -> range y (y + meters) (fun y -> (x, y))
 
         match path with
         | head :: rest -> (next head) @ rest
         | [] -> []
 
-    Seq.fold fold [0,0]
+    Seq.fold fold [ 0, 0 ] >> set
+
+let columns: (int * int) seq -> (int * int Set) seq =
+    let map (col, pts) = (col, pts |> Seq.map snd |> set)
+
+    Seq.groupBy fst >> Seq.map map
+
+type Feature =
+    | Run of int Set
+    | Cross
+
+let crossings: (int * int Set) seq -> (int * (int list)) seq =
+    let fold left right (prev, crossings) =
+        function
+        | y when Set.contains y right && Set.contains y left -> (None, y :: crossings)
+        | y when Set.contains y left && prev = Some Right -> (None, y :: crossings)
+        | y when Set.contains y right && prev = Some Left -> (None, y :: crossings)
+        | y when Set.contains y left -> (Some Left, crossings)
+        | y when Set.contains y right -> (Some Right, crossings)
+        | _ -> (prev, crossings)
+
+    let map =
+        function
+        | [| (_, left); (x, ys); (_, right) |] ->
+            x,
+            Seq.fold (fold left right) (None, []) ys
+            |> snd
+            |> List.sort
+        | _ -> failwith "not enough columns"
+
+    Seq.windowed 3 >> Seq.map map
+
+let isEven n = n % 2 = 0
+
+let between start stop = range (start + 1) (stop - 1) id
+
+let fill border =
+
+    // there isn't enough information in a single column.
+    // if a wall segment column leaves the way it came it did not cross the path counts as 0 or 2
+    // if a wall segment crosses the column it counts as 1
+
+    // turn a column into runs and crosses - need a sliding window of three columns
+
+
+    let columns = columns border
+
+    let crossings = crossings columns |> Ten.p "crossings"
+
+    let rec loop =
+        function
+        | head :: neck :: rest when rest |> List.length |> isEven -> (between head neck) @ loop (neck :: rest)
+        | _ :: rest -> loop rest
+        | [] -> []
+
+
+    let map (x, ys) =
+        let map y = (x, y)
+        ys |> loop |> List.map map |> set
+
+    crossings
+    |> Seq.map map
+    |> Set.unionMany
+    |> Set.union border
+    |> Set.toList
 
 let one lines =
-    parse lines |> Seq.toList |> move |> print |> printfn "%s"
+    parse lines
+    |> Seq.toList
+    |> border
+    |> fill
+    |> print
+    |> printfn "%s"
+
     0
