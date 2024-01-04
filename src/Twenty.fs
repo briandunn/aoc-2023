6af5a12781c6adbb02faf9fb1b9dc78f2b54ce87
Brian Dunn <briandunn@users.noreply.github.com>
2024-01-03 21:49:48 -0600

    initialize conjunctions

--- src/Twenty.fs
+++ src/Twenty.fs
@@ -2,30 +2,49 @@ module Twenty
 
 type Broadcaster = string list
 
-type FlipFlop =
-    { name: string
-      state: bool
-      outputs: string list }
+type FlipFlop = { state: bool }
 
-type Conjunction =
-    { inputStates: Map<string, bool>
-      name: string
-      outputs: string list }
+type Conjunction = { inputStates: Map<string, bool> }
 
-type Module =
-    | Broadcaster of Broadcaster
+type ModuleState =
     | FlipFlop of FlipFlop
     | Conjunction of Conjunction
 
-let parse: string seq -> Module seq =
-    let buildModule moduleType name destinations =
-      let destinations = destinations |> Regex.matches "([a-z]+)" |> Seq.toList
+type Module =
+    { name: string
+      outputs: string list
+      state: ModuleState }
 
-      match moduleType with
-      | "" -> Broadcaster destinations |> Some
-      | "%" -> FlipFlop { name = name; state = false; outputs = destinations } |> Some
-      | "&" -> Conjunction { inputStates = Map.empty; name = name; outputs = destinations } |> Some
-      | _ -> None
+type InputLine =
+    | Broadcaster of Broadcaster
+    | Module of Module
+
+type Pulse =
+    | High
+    | Low
+
+let parse: string seq -> Broadcaster * Map<string, Module> =
+    let buildModule moduleType name destinations =
+        let destinations =
+            destinations
+            |> Regex.matches "([a-z]+)"
+            |> Seq.toList
+
+        match moduleType with
+        | "" -> destinations |> Broadcaster |> Some
+        | "%" ->
+            { name = name
+              outputs = destinations
+              state = FlipFlop { state = false } }
+            |> Module
+            |> Some
+        | "&" ->
+            { name = name
+              outputs = destinations
+              state = Conjunction { inputStates = Map.empty } }
+            |> Module
+            |> Some
+        | _ -> None
 
     let parse =
         Regex.groups "^(%|&)?([a-z]+) -> (.+)$"
@@ -34,9 +53,80 @@ let parse: string seq -> Module seq =
             | [| _; moduleType; name; destinations |] -> buildModule moduleType name destinations
             | _ -> None
 
-    Seq.choose parse
+    let fold (broadcaster, modMap) =
+        function
+        | Broadcaster destinations -> (Some destinations, modMap)
+        | Module ({ name = name } as m) -> broadcaster, (Map.add name m modMap)
 
+    Seq.choose parse
+    >> Seq.fold fold (None, Map.empty)
+    >> function
+        | Some broadcaster, modMap -> broadcaster, modMap
+        | None, _ -> failwith "No broadcaster found"
+
+let initializeConjunctions modules =
+    let conjunctions =
+        let choose (name, m) =
+            match m.state with
+            | Conjunction _ -> Some(name, [])
+            | _ -> None
+
+        modules
+        |> Map.toSeq
+        |> Seq.choose choose
+        |> Map.ofSeq
+
+    let conjunctionInputs =
+        let fold conjunctions (name, { outputs = outputs }) =
+            let fold conjunctions output =
+                match Map.tryFind output conjunctions with
+                | Some outputs -> Map.add output (name :: outputs) conjunctions
+                | None -> conjunctions
+
+            outputs |> List.fold fold conjunctions
+
+        modules |> Map.toSeq |> Seq.fold fold conjunctions
+
+    let fold acc name inputs =
+        let change: Module -> Module =
+            function
+            | { state = Conjunction _ } as m ->
+                { m with state = Conjunction { inputStates = [ for i in inputs -> i, false ] |> Map.ofList } }
+            | m -> m
+
+        Map.change name (Option.map change) acc
+
+    conjunctionInputs |> Map.fold fold modules
 
 let one lines =
-    parse lines |> printfn "%A"
+    let broadcaster, modules = parse lines
+
+    let modules = initializeConjunctions modules
+
+    modules |> Map.iter (printfn "%A %A")
+
+    let rec loop (modules: Map<string, Module>) pulses =
+        match pulses with
+        | [] -> []
+        | (dest, pulse) as head :: rest ->
+            match Map.find dest modules with
+            | { state = FlipFlop { state = state } } as m ->
+                match pulse with
+                | High -> head :: loop modules rest
+                | Low ->
+                    let m = { m with state = FlipFlop { state = not state } }
+                    let modules = Map.add m.name m modules
+                    let outPulse = if state then Low else High
+
+                    let pulses =
+                        m.outputs
+                        |> List.map (fun dest -> (dest, outPulse))
+
+                    head :: loop modules (rest @ pulses)
+            | { state = Conjunction { inputStates = inputStates } } as m -> head :: loop modules rest
+
+    [ for output in broadcaster -> output, Low ]
+    |> loop modules
+    |> printfn "%A"
+
     0
