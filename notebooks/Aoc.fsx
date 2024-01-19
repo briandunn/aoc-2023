cb360468fc751af88a5e756209048d7d9cb104d7
Brian Dunn <briandunn@users.noreply.github.com>
2024-01-19 12:24:55 -0600

    wip 25

--- notebooks/Aoc.fsx
+++ notebooks/Aoc.fsx
@@ -8,3 +8,29 @@ let fetchInput day =
     client.DefaultRequestHeaders.Add("Cookie", sprintf "session=%s" session)
     return! url |> client.GetStringAsync
   } |> Async.AwaitTask |> Async.RunSynchronously
+
+
+type 'a Weights when 'a:comparison = Map<int, 'a list> * Map<'a, int>
+
+module Weights =
+  let empty : 'a Weights = Map.empty, Map.empty
+
+  let add weight point ((pq, map) : 'a Weights) : 'a Weights =
+    let change =
+      function
+      | Some points -> List.distinct (point::points)
+      | None -> [point]
+      >> Some
+    (Map.change weight change pq, Map.add point weight map)
+
+  let tryFind (pt: 'a) ((_, map): 'a Weights) : int option = Map.tryFind pt map
+
+  let rec private pop f ((pq, map) as weights) =
+    if Map.isEmpty pq then None, weights
+    else
+      match f pq with
+      | weight, point::rest -> (Some (weight, point)), (Map.add weight rest pq, Map.remove point map)
+      | weight, [] -> pop f (Map.remove weight pq, map)
+
+  let popMax (weights: 'a Weights): ((int*'a) option)*'a Weights when 'a:comparison = pop Map.maxKeyValue weights
+  let popMin (weights: 'a Weights): ((int*'a) option)*'a Weights when 'a:comparison = pop Map.minKeyValue weights
