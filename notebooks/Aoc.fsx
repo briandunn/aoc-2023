234da5b4714d9fe0cb31baeb5be9453e05d06888
Brian Dunn <briandunn@users.noreply.github.com>
2024-02-14 11:09:07 -0600

    fancier dot

--- notebooks/Aoc.fsx
+++ notebooks/Aoc.fsx
@@ -1,36 +1,63 @@
 module Aoc
 
 let fetchInput day =
-  let session = "53616c7465645f5f10b226a63ffd38a2e4e417f9686924ec98b28a87c982fdb4b2e08aa5c3bb23d2863857450194a13bc6ff565cf1426c7e47de2e52cec63b31"
-  let url = sprintf "https://adventofcode.com/2023/day/%d/input" day
-  task {
-    use client = new System.Net.Http.HttpClient()
-    client.DefaultRequestHeaders.Add("Cookie", sprintf "session=%s" session)
-    return! url |> client.GetStringAsync
-  } |> Async.AwaitTask |> Async.RunSynchronously
+    let session =
+        "53616c7465645f5f10b226a63ffd38a2e4e417f9686924ec98b28a87c982fdb4b2e08aa5c3bb23d2863857450194a13bc6ff565cf1426c7e47de2e52cec63b31"
+
+    let session =
+        "53616c7465645f5f10b226a63ffd38a2e4e417f9686924ec98b28a87c982fdb4b2e08aa5c3bb23d2863857450194a13bc6ff565cf1426c7e47de2e52cec63b31"
+
+    let url = sprintf "https://adventofcode.com/2023/day/%d/input" day
+
+    task {
+        use client = new System.Net.Http.HttpClient()
+        client.DefaultRequestHeaders.Add("Cookie", sprintf "session=%s" session)
+        return! url |> client.GetStringAsync
+    }
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
 
 
-type 'a Weights when 'a:comparison = Map<int, 'a list> * Map<'a, int>
+type 'a Weights when 'a: comparison = Map<int, 'a list> * Map<'a, int>
 
 module Weights =
-  let empty : 'a Weights = Map.empty, Map.empty
+    let empty: 'a Weights = Map.empty, Map.empty
 
-  let add weight point ((pq, map) : 'a Weights) : 'a Weights =
-    let change =
-      function
-      | Some points -> List.distinct (point::points)
-      | None -> [point]
-      >> Some
-    (Map.change weight change pq, Map.add point weight map)
+    let add weight point ((pq, map): 'a Weights) : 'a Weights =
+        let change =
+            function
+            | Some points -> List.distinct (point :: points)
+            | None -> [ point ]
+            >> Some
 
-  let tryFind (pt: 'a) ((_, map): 'a Weights) : int option = Map.tryFind pt map
+        (Map.change weight change pq, Map.add point weight map)
 
-  let rec private pop f ((pq, map) as weights) =
-    if Map.isEmpty pq then None, weights
-    else
-      match f pq with
-      | weight, point::rest -> (Some (weight, point)), (Map.add weight rest pq, Map.remove point map)
-      | weight, [] -> pop f (Map.remove weight pq, map)
+    let tryFind (pt: 'a) ((_, map): 'a Weights) : int option = Map.tryFind pt map
 
-  let popMax (weights: 'a Weights): ((int*'a) option)*'a Weights when 'a:comparison = pop Map.maxKeyValue weights
-  let popMin (weights: 'a Weights): ((int*'a) option)*'a Weights when 'a:comparison = pop Map.minKeyValue weights
+    let rec private pop f ((pq, map) as weights) =
+        if Map.isEmpty pq then
+            None, weights
+        else
+            match f pq with
+            | weight, point :: rest -> (Some(weight, point)), (Map.add weight rest pq, Map.remove point map)
+            | weight, [] -> pop f (Map.remove weight pq, map)
+
+    let popMax (weights: 'a Weights) : ((int * 'a) option) * 'a Weights when 'a: comparison =
+        pop Map.maxKeyValue weights
+
+    let popMin (weights: 'a Weights) : ((int * 'a) option) * 'a Weights when 'a: comparison =
+        pop Map.minKeyValue weights
+
+let dot (graph: string) =
+    let procStart =
+        System.Diagnostics.ProcessStartInfo(
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            FileName = "dot",
+            Arguments = "-Tsvg"
+        )
+
+    let proc = System.Diagnostics.Process.Start procStart
+    proc.StandardInput.Write(graph)
+    proc.StandardInput.Close()
+    proc.StandardOutput.ReadToEnd()
